1d708e4734f68bd2de3a7c758e10e8b7a5cb3044
Lucidity0207 <joshuabowden123@gmail.com>
2020-11-02 14:44:19 +0000

    acknowledge mouse down
    i have made event listeners and functions for mouse down and up. Now the console acknowledges when the mouse is pressed down, this will help me to spawn squares by clicking and drag them on screen.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //variables
 var ctx, controller, square, loop;
-
+var canvas = document.getElementById('mycanvas');
+var offsetLeft, offsetTop
+var isMouseDown = false;
+var mouseX = 0, mouseY = 0;
 //canvas size
 ctx = document.querySelector("canvas").getContext("2d");
 
@@ -17,9 +20,6 @@ square = {
     y: 0,
     y_velocity: 0
 };
-
-
-
 
 
 // making key press functions 
@@ -44,6 +44,19 @@ keyContr = {
     }
 };
 
+canvas.addEventListener('mousedown', onMouseDown);
+canvas.addEventListener('mouseup', onMouseUp);
+
+function onMouseDown(event) {
+    isMouseDown = true;
+    mouseX = event.clientX - offsetLeft;
+    mouseY = event.clientY - offsetTop;
+    console.log('mousedown')
+
+}
+function onMouseUp() {
+    isMouseDown = false;
+}
 loop = function () {
 
     if (keyContr.up && square.jumping == false) {
@@ -57,7 +70,7 @@ loop = function () {
         square.x_velocity += 0.5;
     }
     // physics
-    square.y_velocity += 1.5; //gravity
+    square.y_velocity += 1; //gravity
     square.x += square.x_velocity;
     square.y += square.y_velocity;
     square.x_velocity *= 0.9;// friction
@@ -69,33 +82,33 @@ loop = function () {
         square.y = 325 - 1 - 48;
         square.y_velocity = 0;
     }
-     // if square is going off the left of the screen
-  if (square.x < -64) {
+    // if square is going off the left of the screen
+    if (square.x < -64) {
 
-    square.x = 640;
+        square.x = 640;
 
-  } else if (square.x > 640) {// if square goes past right boundary
+    } else if (square.x > 640) {// if square goes past right boundary
 
-    square.x = -64;
+        square.x = -64;
 
-  }
-  //draw square
-ctx.fillStyle = "#202020";
-ctx.fillRect(0, 0, 640, 325);
-ctx.fillStyle = "#ff0000";
-ctx.beginPath();
-ctx.rect(square.x, square.y, square.width, square.height);
-ctx.fill();
-//floor line
-// ctx.strokeStyle = "#202830";
-// ctx.lineWidth = 4;
-// ctx.beginPath();
-// ctx.moveTo(0, 325);
-// ctx.lineTo(640, 325);
-// ctx.stroke();
+    }
+    //draw square
+    ctx.fillStyle = "#202020";
+    ctx.fillRect(0, 0, 640, 325);
+    ctx.fillStyle = "#ff0000";
+    ctx.beginPath();
+    ctx.rect(square.x, square.y, square.width, square.height);
+    ctx.fill();
+    //floor line
+    // ctx.strokeStyle = "#202830";
+    // ctx.lineWidth = 4;
+    // ctx.beginPath();
+    // ctx.moveTo(0, 325);
+    // ctx.lineTo(640, 325);
+    // ctx.stroke();
 
-//call update when the browser is ready to draw again
-window.requestAnimationFrame(loop);
+    //call update when the browser is ready to draw again
+    window.requestAnimationFrame(loop);
 
 };
 
