cd9e8f48df11c7c9d7c4e4b0a75ddd8cb4b6854e
Lucidity0207 <joshuabowden123@gmail.com>
2020-11-02 13:07:44 +0000

    square and colours
    I created a floor for the player to move the square around on, and also changed the colours of the canvas background so that the floor is more prominent. I also made a square be drawn and set an asset in the square array for when the player can make it jump.

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-var context, controller, rectangle, loop;
+//variables
+var ctx, controller, rectangle, loop;
+
+//canvas size
+ctx = document.querySelector("canvas").getContext("2d");
+
+ctx.canvas.height = 360;
+ctx.canvas.width = 640;
+
+// square (rect)
+square = {
+    height:32,
+    jumping:true,
+    width:32,
+    x:300, //center of canvas
+    vx:0,
+    y:0,
+    vy:0
+};
+//draw square
+ctx.fillStyle = "#202020";
+ctx.fillRect(0, 0, 640, 325);
+ctx.fillStyle ="#ff0000";
+ctx.beginPath();
+ctx.rect(square.x, square.y, square.width, square.height);
+ctx.fill();
+//floor line
+ctx.strokeStyle ="#202830";
+ctx.lineWidth = 4;
+ctx.beginPath();
+ctx.moveTo(0,325);
+ctx.lineTo(640, 325);
+ctx.stroke();
